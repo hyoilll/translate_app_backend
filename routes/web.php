35c69343b4e3a1ccf9fc95d191cf005eac75f008
hyoilll <dlgydlf12345@naver.com>
2024-10-18 01:03:09 +0900

    '/' api 追加

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\TranslatorController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('api/translate', [TranslatorController::class, 'translateText']);
