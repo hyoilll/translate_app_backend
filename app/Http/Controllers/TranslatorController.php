c2ca0454db12f62db6f1c48138a4e3bd5370d701
hyoilll <dlgydlf12345@naver.com>
2024-10-20 16:12:07 +0900

    update controller

--- app/Http/Controllers/TranslatorController.php
+++ app/Http/Controllers/TranslatorController.php
@@ -9,7 +9,15 @@ class TranslatorController extends Controller
     /**
      * 翻訳して返す
      */
-    public function translateText(): string {
-        return 'hello world';
+    public function translateText(Request $req): string {
+        $inputLang = $req->input('inputLang');
+        $outputLang = $req->input('outputLang');
+        $targetText = $req->input('targetText');
+
+        $authKey = config('apiKey.DEEPL_API_KEY');
+        $translator = new \DeepL\Translator($authKey);
+
+        $result = $translator->translateText($targetText, null, $outputLang);
+        return $result->text;
     }
 }
