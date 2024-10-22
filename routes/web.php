f523830009e37f3c765443101d82b2c59fe87a51
hyoilll <dlgydlf12345@naver.com>
2024-10-22 23:16:17 +0900

    api微調整

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\TranslatorController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('api/translate', [TranslatorController::class, 'translateText']);
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('api/translate', [TranslatorController::class, 'translateText']);
