4c60fde9899e569073aa10fed13402ff84191a5b
hyoilll <dlgydlf12345@naver.com>
2024-10-22 23:08:19 +0900

    url微調整

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TranslatorController;
+// use App\Http\Controllers\TranslatorController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/translate', [TranslatorController::class, 'translateText']);
+Route::get('api/translate', [TranslatorController::class, 'translateText']);
