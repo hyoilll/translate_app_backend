8f2c701b56e09a74c1c8d63a03da06e696505c92
hyoilll <dlgydlf12345@naver.com>
2024-10-22 23:04:12 +0900

    微調整

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/translate', [TranslatorController::class, 'translateText']);
+// Route::get('/translate', [TranslatorController::class, 'translateText']);
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-// use App\Http\Controllers\TranslatorController;
+use App\Http\Controllers\TranslatorController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('api/translate', [TranslatorController::class, 'translateText']);
+Route::get('/translate', [TranslatorController::class, 'translateText']);
